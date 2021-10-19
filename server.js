f68b6e48a65273570c5341ecbe04e9b4225c42bb
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-10-19 19:31:36 +0100

    corregir puerto

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ import autorizacionEstadoUsuario from './middleware/autorizacionEstadoUsuario.js
 
 dotenv.config({ path: './.env' });
 
+const port = process.env.PORT || 5000;
+
 const app = Express();
 
 app.use(Express.json());
@@ -43,8 +45,8 @@ app.use(rutasUsuario);
 app.use(rutasVenta);
 
 const main = () => {
-  return app.listen(process.env.PORT, () => {
-    console.log(`escuchando puerto ${process.env.PORT}`);
+  return app.listen(port, () => {
+    console.log(`escuchando puerto ${port}`);
   });
 };
 
